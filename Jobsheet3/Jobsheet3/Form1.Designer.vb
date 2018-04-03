7f3042c4838bb1b0cbcf3e1d80f8bd90ace6e2c6
ahmadlufiau <ahmadlufi300@gmail.com>
2018-04-03 09:16:31 +0700

    Browsing Data Using GridView

--- Jobsheet3/Jobsheet3/Form1.Designer.vb
+++ Jobsheet3/Jobsheet3/Form1.Designer.vb
@@ -22,9 +22,305 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.BtnSimpan = New System.Windows.Forms.Button()
+        Me.BtnBrowse = New System.Windows.Forms.Button()
+        Me.TxtFoto = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.CmbProvinsi = New System.Windows.Forms.ComboBox()
+        Me.CmbKota = New System.Windows.Forms.ComboBox()
+        Me.TxtEmail = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TxtNoHP = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Tgl = New System.Windows.Forms.DateTimePicker()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TxtAlamat = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TxtTLahir = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TxtNama = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtNIM = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(93, 17)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(388, 25)
+        Me.Label11.TabIndex = 52
+        Me.Label11.Text = "FORM PENGISIAN DATA MAHASISWA"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(55, 347)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(456, 164)
+        Me.DataGridView1.TabIndex = 51
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(359, 58)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(152, 176)
+        Me.PictureBox1.TabIndex = 50
+        Me.PictureBox1.TabStop = False
+        '
+        'BtnSimpan
+        '
+        Me.BtnSimpan.Location = New System.Drawing.Point(124, 318)
+        Me.BtnSimpan.Name = "BtnSimpan"
+        Me.BtnSimpan.Size = New System.Drawing.Size(75, 23)
+        Me.BtnSimpan.TabIndex = 49
+        Me.BtnSimpan.Text = "Simpan"
+        Me.BtnSimpan.UseVisualStyleBackColor = True
+        '
+        'BtnBrowse
+        '
+        Me.BtnBrowse.Location = New System.Drawing.Point(346, 291)
+        Me.BtnBrowse.Name = "BtnBrowse"
+        Me.BtnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.BtnBrowse.TabIndex = 48
+        Me.BtnBrowse.Text = "Browse"
+        Me.BtnBrowse.UseVisualStyleBackColor = True
+        '
+        'TxtFoto
+        '
+        Me.TxtFoto.Location = New System.Drawing.Point(124, 292)
+        Me.TxtFoto.Name = "TxtFoto"
+        Me.TxtFoto.Size = New System.Drawing.Size(216, 20)
+        Me.TxtFoto.TabIndex = 47
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(52, 295)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(28, 13)
+        Me.Label10.TabIndex = 46
+        Me.Label10.Text = "Foto"
+        '
+        'CmbProvinsi
+        '
+        Me.CmbProvinsi.FormattingEnabled = True
+        Me.CmbProvinsi.Items.AddRange(New Object() {"Jawa Tengah", "Jawa Timur"})
+        Me.CmbProvinsi.Location = New System.Drawing.Point(124, 213)
+        Me.CmbProvinsi.Name = "CmbProvinsi"
+        Me.CmbProvinsi.Size = New System.Drawing.Size(216, 21)
+        Me.CmbProvinsi.TabIndex = 45
+        '
+        'CmbKota
+        '
+        Me.CmbKota.FormattingEnabled = True
+        Me.CmbKota.Items.AddRange(New Object() {"Kota Semarang", "Kab Semarang", "Kendal", "Demak", "Pati", "Magelang", "Wonosobo", "Purwokerto", "Blora", "Lamongan", "Rambang"})
+        Me.CmbKota.Location = New System.Drawing.Point(124, 187)
+        Me.CmbKota.Name = "CmbKota"
+        Me.CmbKota.Size = New System.Drawing.Size(216, 21)
+        Me.CmbKota.TabIndex = 44
+        '
+        'TxtEmail
+        '
+        Me.TxtEmail.Location = New System.Drawing.Point(124, 266)
+        Me.TxtEmail.Name = "TxtEmail"
+        Me.TxtEmail.Size = New System.Drawing.Size(216, 20)
+        Me.TxtEmail.TabIndex = 43
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(52, 269)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(32, 13)
+        Me.Label9.TabIndex = 42
+        Me.Label9.Text = "Email"
+        '
+        'TxtNoHP
+        '
+        Me.TxtNoHP.Location = New System.Drawing.Point(124, 240)
+        Me.TxtNoHP.Name = "TxtNoHP"
+        Me.TxtNoHP.Size = New System.Drawing.Size(216, 20)
+        Me.TxtNoHP.TabIndex = 41
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(52, 243)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 40
+        Me.Label7.Text = "No HP"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(52, 217)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(44, 13)
+        Me.Label8.TabIndex = 39
+        Me.Label8.Text = "Provinsi"
+        '
+        'Tgl
+        '
+        Me.Tgl.Location = New System.Drawing.Point(124, 137)
+        Me.Tgl.Name = "Tgl"
+        Me.Tgl.Size = New System.Drawing.Size(216, 20)
+        Me.Tgl.TabIndex = 38
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(52, 191)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(29, 13)
+        Me.Label5.TabIndex = 37
+        Me.Label5.Text = "Kota"
+        '
+        'TxtAlamat
+        '
+        Me.TxtAlamat.Location = New System.Drawing.Point(124, 162)
+        Me.TxtAlamat.Name = "TxtAlamat"
+        Me.TxtAlamat.Size = New System.Drawing.Size(216, 20)
+        Me.TxtAlamat.TabIndex = 36
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(52, 165)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.TabIndex = 35
+        Me.Label6.Text = "Alamat"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(52, 139)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 13)
+        Me.Label3.TabIndex = 34
+        Me.Label3.Text = "Tanggal Lahir"
+        '
+        'TxtTLahir
+        '
+        Me.TxtTLahir.Location = New System.Drawing.Point(124, 110)
+        Me.TxtTLahir.Name = "TxtTLahir"
+        Me.TxtTLahir.Size = New System.Drawing.Size(216, 20)
+        Me.TxtTLahir.TabIndex = 33
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(52, 113)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(69, 13)
+        Me.Label4.TabIndex = 32
+        Me.Label4.Text = "Tempat Lahir"
+        '
+        'TxtNama
+        '
+        Me.TxtNama.Location = New System.Drawing.Point(124, 84)
+        Me.TxtNama.Name = "TxtNama"
+        Me.TxtNama.Size = New System.Drawing.Size(216, 20)
+        Me.TxtNama.TabIndex = 31
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(52, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 30
+        Me.Label2.Text = "Nama"
+        '
+        'TxtNIM
+        '
+        Me.TxtNIM.Location = New System.Drawing.Point(124, 58)
+        Me.TxtNIM.Name = "TxtNIM"
+        Me.TxtNIM.Size = New System.Drawing.Size(216, 20)
+        Me.TxtNIM.TabIndex = 29
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(52, 61)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 13)
+        Me.Label1.TabIndex = 28
+        Me.Label1.Text = "NIM"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(562, 518)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.BtnSimpan)
+        Me.Controls.Add(Me.BtnBrowse)
+        Me.Controls.Add(Me.TxtFoto)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.CmbProvinsi)
+        Me.Controls.Add(Me.CmbKota)
+        Me.Controls.Add(Me.TxtEmail)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TxtNoHP)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Tgl)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TxtAlamat)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TxtTLahir)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TxtNama)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TxtNIM)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label11 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents BtnSimpan As Button
+    Friend WithEvents BtnBrowse As Button
+    Friend WithEvents TxtFoto As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents CmbProvinsi As ComboBox
+    Friend WithEvents CmbKota As ComboBox
+    Friend WithEvents TxtEmail As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TxtNoHP As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Tgl As DateTimePicker
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TxtAlamat As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TxtTLahir As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TxtNama As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxtNIM As TextBox
+    Friend WithEvents Label1 As Label
 End Class
--- Jobsheet3/Jobsheet3/Form1.vb
+++ Jobsheet3/Jobsheet3/Form1.vb
@@ -1,3 +1,81 @@
-﻿Public Class Form1
+﻿Imports System.Data.OleDb
+Public Class Form1
 
+    'Browsing Data Using DataGridView
+
+    Dim cnnOLEDB As New OleDbConnection
+    Dim cmdOLEDB As New OleDbCommand
+    Dim strConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" &
+        System.Environment.CurrentDirectory & "\Akademik.accdb"
+    Public ADP As OleDbDataAdapter
+    Public DS As New DataSet
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        GetData(e)
+    End Sub
+
+    Private Sub DataGridView1_CellContextMenuStripChanged(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContextMenuStripChanged
+        GetData(e)
+    End Sub
+
+    Sub GetData(e)
+        'Mendeklarasikan variabel sebagai Objek dan mengambil nilai pada 
+        'setiap baris tabel yang dipilih 
+        Dim NIM As Object = DataGridView1.Rows(e.RowIndex).Cells(0).Value
+        Dim Nama As Object = DataGridView1.Rows(e.RowIndex).Cells(1).Value
+        Dim TempatLahir As Object = DataGridView1.Rows(e.RowIndex).Cells(2).Value
+        Dim TanggalLahir As Object = DataGridView1.Rows(e.RowIndex).Cells(3).Value
+        Dim Alamat As Object = DataGridView1.Rows(e.RowIndex).Cells(4).Value
+        Dim Kota As Object = DataGridView1.Rows(e.RowIndex).Cells(5).Value
+        Dim Provinsi As Object = DataGridView1.Rows(e.RowIndex).Cells(6).Value
+        Dim NoHP As Object = DataGridView1.Rows(e.RowIndex).Cells(7).Value
+        Dim Email As Object = DataGridView1.Rows(e.RowIndex).Cells(8).Value
+
+        'Mengubah tipe variabel yang diambil yang berbentuk objek 
+        'menjadi berbentuk String dan menampilkan pada TextBox
+        TxtNIM.Text = CType(NIM, String)
+        TxtNama.Text = CType(Nama, String)
+        TxtTLahir.Text = CType(TempatLahir, String)
+        Tgl.Text = CType(TanggalLahir, String)
+        TxtAlamat.Text = CType(Alamat, String)
+        TxtNoHP.Text = CType(NoHP, String)
+        CmbKota.Text = CType(Kota, String)
+        CmbProvinsi.Text = CType(Provinsi, String)
+        TxtEmail.Text = CType(Email, String)
+
+        'Menambahkan parameter untuk mengambil data foto dari database
+        Try
+            Dim command As New OleDbCommand("SELECT Foto FROM Master_Mahasiswa WHERE NIM=@NIM", cnnOLEDB)
+            command.Parameters.AddWithValue("@NIM", CType(NIM, String))
+            'Deklarasi pengambilan file Foto sebagai file Byte
+            Dim pictureData As Byte() = DirectCast(command.ExecuteScalar(), Byte())
+            command.Dispose()
+            'Mengubah file byte foto menjadi stream
+            Dim Stream As New IO.MemoryStream(pictureData)
+            'Menampilkan foto pada PictureBox
+            Me.PictureBox1.Image = Image.FromStream(Stream)
+            Stream.Dispose()
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cnnOLEDB.ConnectionString = strConnectionString
+        cnnOLEDB.Open()
+        TampilData()
+        TxtNIM.MaxLength = 12
+        TxtNama.MaxLength = 30
+        TxtTLahir.MaxLength = 20
+        TxtAlamat.MaxLength = 50
+        TxtNoHP.MaxLength = 13
+        TxtEmail.MaxLength = 30
+    End Sub
+
+    Sub TampilData()
+        ADP = New OleDbDataAdapter("SELECT * FROM Master_Mahasiswa ORDER BY NIM", cnnOLEDB)
+        DS = New DataSet
+        ADP.Fill(DS, "Tabel1")
+        DataGridView1.DataSource = DS.Tables("Tabel1")
+    End Sub
 End Class
